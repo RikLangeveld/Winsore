a9743670d65a1c28f217a78dfb908fafc1630269
RikLangeveld <rik_langeveld@hotmail.com>
2016-03-04 17:10:43 +0100

    Beginnetje aan de character movement gemaakt

--- Winsore/Winsore/Winsore/GameWorld.cs
+++ Winsore/Winsore/Winsore/GameWorld.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameManagement.gameobjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,17 @@ namespace Winsore
     class GameWorld : GameObjectList
     {
         private SpriteGameObject background = null;
+        private Player player;
 
         public GameWorld()
         {
             background = new SpriteGameObject("grass");
-
+            player = new Player("spr_hero_placeholder");
+            
             // Add the grass background to the gameWorld
             this.Add(background);
+
+            this.Add(player);
         }
     }
 }
---
+++ Winsore/Winsore/Winsore/Player.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameManagement.gameobjects
+{
+    class Player : SpriteGameObject
+    {
+        public Player(string assetname) : base(assetname)
+            {
+            position = new Vector2(100, 100);
+            }
+    }
+}
