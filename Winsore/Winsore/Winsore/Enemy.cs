6e9c335c898d4e790295b199ea0316ec0994ebed
kayvdlans <kayvanderlans@gmail.com>
2016-03-08 09:22:28 +0100

    Add Standard Enemy
    NOT
    DONE
    YET!!!!!!!!!!!

---
+++ Winsore/Winsore/Winsore/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winsore
+{
+    class Enemy : SpriteGameObject
+    {
+        protected int health;
+        protected float damageToUnits;
+        protected float damageToWall;
+
+        public Enemy(string assetname) : base(assetname)
+        {
+            position = new Vector2(0, 600);
+            CalculateRandomVelocity(25, 100);
+            Health = 100;
+        }
+
+        public void CalculateRandomVelocity(int minValue, int maxValue)
+        {
+            velocity.X = GameEnvironment.Random.Next(minValue, maxValue);
+        }
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+                if (health <= 0)
+                    visible = false;
+            }
+        }
+    }
+}
--- Winsore/Winsore/Winsore/GameWorld.cs
+++ Winsore/Winsore/Winsore/GameWorld.cs
@@ -1,4 +1,6 @@
 ﻿using GameManagement.gameobjects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +12,34 @@ namespace Winsore
     {
         private SpriteGameObject background = null;
         private Player player;
+        private Enemy enemy, enemy2, enemy3;
 
         public GameWorld()
         {
             background = new SpriteGameObject("grass");
             player = new Player("spr_hero_placeholder");
+            enemy = new Enemy("spr_enemy_placeholder");
+            enemy2 = new Enemy("spr_enemy_placeholder");
+            enemy3 = new Enemy("spr_enemy_placeholder");
             
             // Add the grass background to the gameWorld
             this.Add(background);
 
             this.Add(player);
+            Add(enemy);
+            Add(enemy2);
+            Add(enemy3);
+            enemy2.Position = new Vector2(0, 500);
+            enemy3.Position = new Vector2(0, 400);
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            if (inputHelper.KeyPressed(Keys.A))
+            {
+                enemy.Health -= 20;
+                Console.WriteLine(enemy.Health);
+            }
         }
     }
 }
