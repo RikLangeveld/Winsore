782cb01130e89b00b5e700a2cd58f42b98e4d6ac
kayvdlans <kayvanderlans@gmail.com>
2016-03-08 10:42:22 +0100

    Comment Enemy Code
    And removed 2 Enemies, since they were only for debugging purposes.

--- Winsore/Winsore/Winsore/Enemy.cs
+++ Winsore/Winsore/Winsore/Enemy.cs
@@ -12,6 +12,7 @@ namespace Winsore
         protected int health;
         protected float damageToUnits;
         protected float damageToWall;
+        //TO-DO: Add Aggro Range for units and player
 
         public Enemy(string assetname) : base(assetname)
         {
@@ -20,17 +21,22 @@ namespace Winsore
             Health = 100;
         }
 
+        /// <summary>
+        /// Calculates a random horizontal velocity between the Minimum Value and Maximum Value provided.
+        /// </summary>
+        /// <param name="minValue">The minimal velocity of the enemy</param>
+        /// <param name="maxValue">The maximal velocity of the enemy</param>
         public void CalculateRandomVelocity(int minValue, int maxValue)
         {
             velocity.X = GameEnvironment.Random.Next(minValue, maxValue);
         }
 
+        /// <summary>
+        /// Return the health of the enemy or Set the health to a certain amount.
+        /// </summary>
         public int Health
         {
-            get
-            {
-                return health;
-            }
+            get { return health; }
             set
             {
                 health = value;
--- Winsore/Winsore/Winsore/GameWorld.cs
+++ Winsore/Winsore/Winsore/GameWorld.cs
@@ -12,34 +12,19 @@ namespace Winsore
     {
         private SpriteGameObject background = null;
         private Player player;
-        private Enemy enemy, enemy2, enemy3;
+        private Enemy enemy;
 
         public GameWorld()
         {
             background = new SpriteGameObject("grass");
             player = new Player("spr_hero_placeholder");
             enemy = new Enemy("spr_enemy_placeholder");
-            enemy2 = new Enemy("spr_enemy_placeholder");
-            enemy3 = new Enemy("spr_enemy_placeholder");
             
             // Add the grass background to the gameWorld
-            this.Add(background);
+            Add(background);
 
-            this.Add(player);
+            Add(player);
             Add(enemy);
-            Add(enemy2);
-            Add(enemy3);
-            enemy2.Position = new Vector2(0, 500);
-            enemy3.Position = new Vector2(0, 400);
-        }
-
-        public override void HandleInput(InputHelper inputHelper)
-        {
-            if (inputHelper.KeyPressed(Keys.A))
-            {
-                enemy.Health -= 20;
-                Console.WriteLine(enemy.Health);
-            }
         }
     }
 }
