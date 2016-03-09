c6c91c9d40bd6632441b6772c597f81da6da4eaf
RikLangeveld <rik_langeveld@hotmail.com>
2016-03-09 13:04:25 +0100

    GameWorld functie's

--- Winsore/Winsore/Winsore/GameWorld.cs
+++ Winsore/Winsore/Winsore/GameWorld.cs
@@ -9,7 +9,7 @@ namespace Winsore
 {
     class GameWorld : GameObjectList
     {
-        private SpriteGameObject background = null;
+        private SpriteGameObject background;
         private Player player;
 
         public GameWorld()
@@ -19,10 +19,17 @@ namespace Winsore
 
             // Add the grass background to the gameWorld
             this.Add(background);
-
+            // Add the player to the GameObjectList of gameWorld
             this.Add(player);
         }
 
+        /// <summary>
+        /// A function to check if a spriteGameobject is Outside The room
+        /// </summary>
+        /// <param name="position">The position of the game object</param>
+        /// <param name="width"> The width of the SpriteObject</param>
+        /// <param name="height">The heigth of the SpriteObject</param>
+        /// <returns></returns>
         public bool IsOutsideRoom(Vector2 position, int width, int height)
         {
 
@@ -32,5 +39,62 @@ namespace Winsore
             else
                 return true;
         }
+
+        /// <summary>
+        /// Used to check if a object is out of the room on the right side
+        /// </summary>
+        /// <param name="position">position of the object</param>
+        /// <param name="width">width of the object</param>
+        /// <returns></returns>
+        public bool IsOutsideRoomRight(Vector2 position, int width)
+        {
+            if (position.X + width > Winsore.Screen.X)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Used to check if a object is out of the room on the left side
+        /// </summary>
+        /// <param name="position">position of the object</param>
+        /// <param name="width">width of the object</param>
+        /// <returns></returns>
+        public bool IsOutsideRoomLeft(Vector2 position)
+        {
+            if (position.X < 0)
+                return true;
+            else
+                return false;            
+        }
+
+        /// <summary>
+        /// Used to check if a object is below the view.
+        /// </summary>
+        /// <param name="position">position of the object</param>
+        /// <param name="width">width of the object</param>
+        /// <returns></returns>
+        public bool IsOutsideRoomBelow(Vector2 position, int height)
+        {
+            if (position.Y + height > Winsore.Screen.Y)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Used to check if a object is above the view.
+        /// </summary>
+        /// <param name="position">position of the object</param>
+        /// <param name="width">width of the object</param>
+        /// <returns></returns>
+        public bool IsOutsideRoomAbove (Vector2 position, int height)
+        {
+            if (position.Y <0 )
+                return true;
+            else
+                return false;
+        }
+
     }
 }
