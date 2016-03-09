dba2003b493dd16c838506d25a589ef2c87edda7
AjAxRiCoO8 <Ricardo-42-ajax@live.nl>
2016-03-09 14:12:18 +0100

    Shop + upgrade klassen
    Heel erg weinig aan gedaan. Is meer een template om later te gebruiken.

---
+++ Winsore/Winsore/Winsore/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winsore
+{
+    class Shop
+    {
+
+        List<Upgrade> upgrades;
+
+        public Shop()
+        {
+            upgrades = new List<Upgrade>();
+
+            upgrades.Add(new Upgrade(new Player("spr_hero_placeholder")));
+        }
+    }
+}
---
+++ Winsore/Winsore/Winsore/Upgrade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winsore
+{
+    class Upgrade
+    {
+        SpriteGameObject type;
+
+
+        public Upgrade(SpriteGameObject type)
+        {
+
+        }
+    }
+}
