260056ca54b082bda13d6a1ae489b5ef58a2e825
Wouter Vos <wp.vos@live.nl>
2016-03-23 13:02:29 +0100

    projectile

--- Winsore/Winsore/Winsore/Classes/Projectile.cs
+++ Winsore/Winsore/Winsore/Classes/Projectile.cs
@@ -9,69 +9,17 @@ namespace Winsore
 {
     class Projectile : SpriteGameObject
     {
-        int projectileSpeed = 200; //STC
 
-        protected float angle;
-        
-        protected bool arrowOutsideRoom; // Boolean die false is als de arrow buiten de room is.
-        protected Vector2 lastPositionInsideRoom; // This is used to prefent the player from leaving the view
+        public Vector2 position;
+        public Vector2 velocity;
+        public Vector2 origin;
 
-        private bool shootingArrow;
-        private bool enemyHit;
+        public bool isVisible;
 
-        public Projectile(string assetname) 
+        public Projectile(string assetname)
             : base(assetname)
         {
-            //reset de arrow bij het starten van spel zodat je meteen kan schieten
-            Reset();
+            isVisible = false;
         }
-
-        //TODO Handle input voor schieten van projectile(arrow)
-        public override void HandleInput(InputHelper input)
-        {
-            GameWorld pgw = GameWorld as GameWorld;
-
-            if (input.IsKeyDown(Keys.Space) && !arrowOutsideRoom) //(!arrowOutsideRoom || !enemyHit))
-                shootingArrow = true;
-            else if (!input.IsKeyDown(Keys.Space))
-                shootingArrow = false;
-
-            /*double opposite = inputHelper.MousePosition.Y - position.Y;
-            double adjacent = inputHelper.MousePosition.X - position.X;
-            angle = (float)Math.Atan2(opposite, adjacent);*/
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            if (shootingArrow)
-            {
-                velocity.X += projectileSpeed;
-                velocity.Y += projectileSpeed;
-            }
-
-            if (!arrowOutsideRoom || enemyHit)
-            {
-                //Reset();
-            }
-
-                base.Update(gameTime);
-
-        }
-
-        //TODO Reset methode die de arrow reset op de player zodra deze buiten scherm is of een enemy raakt.
-        public override void Reset()
-        {
-            base.Reset();
-            angle = 0f;
-            velocity = Vector2.Zero;
-
-            position = new Vector2(0, 0);
-            // Reset naar positie van player
-            //position = new Vector2(?, ?); player positie
-            //position = Player.Position
-
-            //shootingArrow = false;
-        }
-    
     }
 }
---
+++ Winsore/Winsore/Winsore/Classes/ProjectileManager.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winsore.Classes
+{
+    class ProjectileManager : GameObjectList
+    {
+        //List<Projectile> projectiles = new List<Projectile>();
+        public Vector2 distance;
+        public float rotation;
+
+        public ProjectileManager()
+        {
+
+        }
+
+        public override void HandleInput(InputHelper input)
+        {
+            GameWorld pgw = GameWorld as GameWorld;
+
+            if (input.IsKeyDown(Keys.Space))
+            {
+                //empty
+                Shoot();
+            }
+                
+        }
+
+        public void UpdateProjectile()
+        {
+            foreach(Projectile projectile in gameObjects)
+            {
+                projectile.position += projectile.velocity;
+
+                //TODO
+                //if statement met out of screen 
+                // dan isVisible = false
+                //if ()                                        
+                //    projectile.isVisible = false;
+
+                
+
+                
+            }
+
+            for(int i = 0; i < gameObjects.Count; i++)
+            {
+                if (!(gameObjects[i] as Projectile).isVisible)
+                {
+                    gameObjects.RemoveAt(i);
+                    //i--;
+                }
+            }
+        }
+
+        public void Shoot()
+        {
+            Projectile newProjectile = new Projectile("arrow_projectile");
+            newProjectile.velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation)) * 5f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            MouseState mouse = Mouse.GetState();
+
+            GameWorld pgw = GameWorld as GameWorld;
+
+            distance.X = mouse.X - pgw.Player.Position.X;
+            distance.Y = mouse.Y - pgw.Player.Position.Y;
+
+            rotation = (float)Math.Atan2(distance.Y, distance.X);
+        }
+
+    }
+}
